54b91af23c4af37e54bd65a236b69227b2972be9
Irina Tomescu <irina.tomescu@green.local>
2018-07-30 18:37:59 +0300

    wip // * work on portfolio

--- js/modals.js
+++
@@ -1,6 +0,0 @@
-$("#modalInterview5").on('hidden.bs.modal', function (e) {
-    $("#modalInterview5 iframe").attr("src", $("#modalInterview5 iframe").attr("src"));
-});
-$("#modalInterview6").on('hidden.bs.modal', function (e) {
-    $("#modalInterview6 iframe").attr("src", $("#modalInterview6 iframe").attr("src"));
-});
